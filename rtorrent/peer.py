cb6d47aea209c227a07fe24080fac45764411d91
Chris Lucas <chris@chrisjlucas.com>
2011-12-27 21:50:56 -0700

    updated year/contact info in copyright notice

--- rtorrent/peer.py
+++ rtorrent/peer.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2011 Chris Lucas, <cjlucas07@gmail.com>
+# Copyright (c) 2012 Chris Lucas, <chris@chrisjlucas.com>
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
 # "Software"), to deal in the Software without restriction, including
